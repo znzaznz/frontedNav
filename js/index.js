ae76eb5fd9c0986dc1a066f9b19fc8458465449e
xiao <779740337@qq.com>
2020-11-21 17:36:09 +0800

    将render实现

--- js/index.js
+++ js/index.js
@@ -1,47 +1,60 @@
 // let hashArr = window.localStorage.key(1)
 $(document).ready(function (){
-    let hashMap;
-    if (localStorage.getItem("hashMap")){
-       hashMap = localStorage.getItem("hashMap");
-       hashMap = hashMap.split(",")
+    // 从当地的地址中取到url,并渲染
+    let hashMap = getUrl();
+    render(hashMap);
+
+    // 给每个li绑定事件
+    $(".iconAdd").on("click",()=>{
+        let url = window.prompt("请输入您要加入的网址");
+        hashMap.push(resolveUrl(url))
+        render(hashMap);
+        localStorage.setItem("hashMap",JSON.stringify(hashMap))
+    })
+})
+
+// 从当地的localStorage中取出数组并做好处理
+function getUrl(){
+    let hashMap = localStorage.getItem("hashMap");
+    if (hashMap){
+        hashMap = JSON.parse(localStorage.getItem("hashMap"));
     }else{
         hashMap = ["https://www.acfun.cn/","https://www.bilibili.com/"]
-        localStorage.setItem("hashMap",hashMap);
+        localStorage.setItem("hashMap",JSON.stringify(hashMap));
     }
-    hashMap.forEach(
-        (value)=>{
-            let valueUrl = resolutionOfDomain(value)
-            $(`<li>
-                <a href="${value}">
+
+    console.log(hashMap)
+    return hashMap
+}
+
+// 将用户传入的url作一个处理,如果符合条件返回一个合适的网址
+function resolveUrl(url){
+    if (url.indexOf("http") === -1 ){
+        url = "https://"+url
+    }
+    return url
+}
+//渲染数组
+function render(arr){
+    console.log($(".siteGroup").find("li:not(.iconAdd)").remove());
+    debugger
+    arr.forEach((url)=>{
+        let valueUrl =  resolutionOfDomain(url);
+        $(`<li>
+                <a href="${url}">
                     <div class="iconSite">${valueUrl[0]}</div>
                     <div class="iconTags">${valueUrl}</div>
                 </a>
             </li>`).insertBefore(".iconAdd")
-            console.log("输入完成");
         }
-        )
-    $(".iconAdd").on("click",()=>{
-        let url = window.prompt("请输入您要加入的网址");
-        if (url.indexOf("http") === -1 ){
-            url = "https://"+url
-        }
-        const urlValue = resolutionOfDomain(url);
-        $(`<li>
-                <a href="${url}">
-                    <div class="iconSite">${urlValue[0]}</div>
-                    <div class="iconTags">${urlValue}</div>
-                </a>
-            </li>`).insertBefore(".iconAdd");
-        hashMap.push(url)
-        localStorage.setItem("hashMap",hashMap)
-    })
-})
-
+    )
+}
+// 处理li上的域名的显示
 function resolutionOfDomain(url){
     if (url.search("www") !== -1){
-        console.log(url.split("."))
        return  url.split(".")[1]
-    }else if (url.search("http") !== -1){
+    }
+    if (url.search("http") !== -1){
         return url.split("//")[1]
     }
 }
